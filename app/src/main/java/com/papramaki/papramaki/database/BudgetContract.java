65c6a02942fd0e7d747d5db20dbdfe43018fad44
Paul Chery <paulachery@gmail.com>
2016-03-21 00:05:45 -0500

    "Added database contact and helper"

---
+++ app/src/main/java/com/papramaki/papramaki/database/BudgetContract.java
@@ -0,0 +1,46 @@
+package com.papramaki.papramaki.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by paulchery on 3/19/16.
+ */
+public final class BudgetContract {
+
+
+    public BudgetContract(){}
+
+
+    public static abstract class Budget implements BaseColumns {
+        public static final String TABLE_NAME = "budget";
+        public static final String COLUMN_NAME_BUDGET_ID = "id";
+        public static final String COLUMN_NAME_AMOUNT = "amount";
+        //public static final String COLUMN_NAME_CREATION_DATE = "date";
+        //public static final String COLUMN_NAME_HISTORY = "history";
+        public static final String COLUMN_NAME_NULLABLE = null;
+        private static final String TEXT_TYPE = " TEXT";
+        private static final String REAL_TYPE = " REAL";
+        private static final String BLOB_TYPE = " BLOB";
+        private static final String COMMA_SEP = ",";
+
+        //Should these two be public?
+        public static final String SQL_CREATE_ENTRIES =
+                "CREATE TABLE " + Budget.TABLE_NAME + " (" +
+                        Budget.COLUMN_NAME_BUDGET_ID + " INTEGER PRIMARY KEY, " +
+                        //Budget.COLUMN_NAME_BUDGET_ID + TEXT_TYPE + COMMA_SEP +
+                        Budget.COLUMN_NAME_AMOUNT + REAL_TYPE + COMMA_SEP +
+                        //Budget.COLUMN_NAME_CREATION_DATE + TEXT_TYPE + COMMA_SEP +
+                        //Budget.COLUMN_NAME_HISTORY + BLOB_TYPE +
+                        " )";
+
+        public static final String SQL_DELETE_ENTRIES =
+                "DROP TABLE IF EXISTS " + Budget.TABLE_NAME;
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/papramaki/papramaki/database/DatabaseHelper.java
@@ -0,0 +1,85 @@
+package com.papramaki.papramaki.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by paulchery on 3/20/16.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Budget.db";
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database){
+        database.execSQL(BudgetContract.Budget.SQL_CREATE_ENTRIES);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL(BudgetContract.Budget.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public void addBudget(double budget){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(BudgetContract.Budget.COLUMN_NAME_AMOUNT, budget); // budget amount
+
+        if(values != null) {
+            // Inserting Row
+            db.insert(BudgetContract.Budget.TABLE_NAME, null, values);
+        }
+        db.close(); // Closing database connection
+    }
+
+    public double viewLatestBudget(){
+        String LATEST_BUDGET =
+            "SELECT * FROM " + BudgetContract.Budget.TABLE_NAME +
+                    " WHERE _id = (SELECT MAX(_id) FROM " +
+                    BudgetContract.Budget.TABLE_NAME + ");";
+
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(LATEST_BUDGET, null);
+        cursor.moveToLast();
+        //why does second index work? what is in index 1?
+        return cursor.getDouble(2);
+    }
+
+    public List<Double> viewBudgets() {
+        List<Double> budgetList = new ArrayList<Double>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + BudgetContract.Budget.TABLE_NAME;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                double amount = cursor.getDouble(2);
+                // Adding contact to list
+                budgetList.add(amount);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return budgetList;
+    }
+
+}
