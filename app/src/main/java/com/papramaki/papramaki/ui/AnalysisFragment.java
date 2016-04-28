c3fc8761e2cd513e3c9b85848e00ae3a9ab7a4ba
Paul Chery <paulachery@gmail.com>
2016-04-28 15:56:10 -0500

    merge conflict

--- app/src/main/java/com/papramaki/papramaki/ui/AnalysisFragment.java
+++ app/src/main/java/com/papramaki/papramaki/ui/AnalysisFragment.java
@@ -199,10 +199,8 @@ public class AnalysisFragment extends Fragment {
 //        }
         for (Category category : categories){
             if (!expensesMap.containsKey(category.getName())) {
-                System.out.println("//////////////////////////////////ANALYSIS1= " + category.getSumCategory());
                 expensesMap.put(category.getName(), category.getSumCategory());
             } else {
-                System.out.println("//////////////////////////////////ANALYSIS2");
                 expensesMap.put(category.getName(), expensesMap.get(category.getName()) + category.getSumCategory());
             }
         }
